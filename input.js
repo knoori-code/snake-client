7e4bb20da1e7973ab25762d5f1f2e4d078213b87
knoori-code <knoori3@gmail.com>
2023-01-18 18:04:11 -0500

    Moved user input function to input.js and imported setupInput function to play.js

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume()
+
+  // \u0003 maps to ctrl+c
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+module.exports = setupInput
--- play.js
+++ play.js
@@ -1,25 +1,10 @@
 const connect = require("./client")
+const setupInput = require("./input")
 
 console.log("Connecting...");
 
 connect()
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf-8");
-  stdin.resume()
 
-  // \u0003 maps to ctrl+c
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-}
 
 setupInput()
