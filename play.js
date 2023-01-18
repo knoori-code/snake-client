549dad563c47f5fde63c7724227f90ebec9c56b4
knoori-code <knoori3@gmail.com>
2023-01-18 15:48:26 -0500

    Initial commit of play.js

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// Establishes a connection with the game server
+const connect = function () {
+
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding("utf-8")
+
+  conn.on("data", (message) => {
+    console.log(message)
+  })
+
+  return conn;
+
+}
+
+// created conn object using the net library. Represents connection with the server. 
+
+console.log("Connecting...");
+
+connect()
