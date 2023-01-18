b94d681029ad608fe60848ffaaf0708bed39a67e
knoori-code <knoori3@gmail.com>
2023-01-18 17:52:41 -0500

    Added setupInput function inside play.js to handle user inputs

--- play.js
+++ play.js
@@ -3,3 +3,23 @@ const connect = require("./client")
 console.log("Connecting...");
 
 connect()
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume()
+
+  // \u0003 maps to ctrl+c
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+setupInput()
